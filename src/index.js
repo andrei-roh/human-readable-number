e6e8f8c436bf8d20fcfc0bb3185fafaacb97125d
Andrei Roh <andrei_roh@tut.by>
2020-09-29 14:46:51 +0300

    Init: human readable number task
    Added function for translation numbers in words

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+  const readableNumbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const readableDozens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  let n = number.toString();
+  if (number === 0){
+    return number = 'zero'
+  }
+  else if (number < 20){
+    return number = readableNumbers[number]
+  }
+  else if (number < 100){
+    return number = (readableDozens[parseInt(number / 10)] + ' ' + readableNumbers[parseInt(number % 10)]).trim()
+  }
+  else if (number >= 100) {
+    if (parseInt(number%100) < 20){
+    return number = (readableNumbers[parseInt(number / 100)] + ' hundred ' + readableNumbers[parseInt(n.substring(1))]).trim()
+    }
+    else {
+    return number = (readableNumbers[parseInt(number / 100)] + ' hundred ' + readableDozens[parseInt(n[1])] + ' ' + readableNumbers[parseInt(n[2])]).trim()
+    }
+  }
 }
